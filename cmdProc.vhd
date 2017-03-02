aa1c98c92848e1a3665e338e51ee5350c86e2eef
Exelion <dw14089@my.bristol.ac.uk>
2017-03-02 15:46:51 +0000

    State machine

--- cmdProc.vhd
+++ cmdProc.vhd
@@ -1,11 +1,11 @@
---Command Processor 
+--Command Processor
 --Daryl White and Alexander Hamilton
 library ieee;
 use ieee.std_logic_1164.all;
 
 --code goes here
 
-entity cmdProc is	 
+entity cmdProc is
  port(
   CLK: in std_logic;
   reset: in std_logic;
@@ -29,26 +29,26 @@ entity cmdProc is
 	txDone: in std_logic
  );
 end;
- 
+
 architecture cmdProc_arch of cmdProc is
   type state_type is (init, read, send);
   -- Init is the cmd block waitning for the signal, read is reading the data from the rx
   -- Send is sending the instructions to the data processor
   signal curState, nextState: state_type;
   --input Signals
-  
-  
+
+
 begin
 
   stateReg: process(clk, reset)
-  begin 
+  begin
     if reset ='1' then
       curState <= init;
     elsif clk 'event and clk ='1' then
       curState <= nextState;
     end if;
   end process;
-  
+
   stateOrder: process(curState)
   begin
     case curState is
@@ -71,8 +71,9 @@ end;
 architecture cmdProc_Rx of cmdProc is
 	state_type is (S0, S1, S2, S3, S4); --States go here
 	--Signals
-	signal curState, nextState: state_type;	
-begin	
+	signal curState, nextState: state_type;
+  signal rxDs: std_logic;
+begin
 
 
 seq_state: process (CLK, reset) --changes state on clock
@@ -85,16 +86,18 @@ begin
 end process; -- seq
 
 
-rxStateMachine: process(curState) -- process rxStateMachine sensitivity
+rxStateMachine: process(curState,rxD) -- process rxStateMachine sensitivity
 begin
 	case curState is
 		when S0 =>
-		--do s0
+      if valid = '1';
+		    nextState <= S1
+      else nextState <= S0;
 		when S1 =>
-		--do s1
+		  if rxD = "a"
 		
-		
-end process; 
+
+end process;
 
 combi_out: process(curState)
  begin
@@ -103,12 +106,3 @@ combi_out: process(curState)
      Y <= '1';
    end if;
  end process; -- combi_output
-
-
-
-
-
-
-
-
-  
